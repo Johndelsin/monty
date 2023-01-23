98d833103ef0f5669ac720b0ad666e29ad8a5b7f
Johndelsin <johndelsin22@gmail.com>
2023-01-22 20:21:46 -0800

    Task All

--- monty.h
+++ monty.h
@@ -17,21 +17,13 @@
 #include <ctype.h>
 
 /**
-
  * struct stack_s - doubly linked list representation of a stack (or queue)
-
  * @n: integer
-
  * @prev: points to the previous element of the stack (or queue)
-
  * @next: points to the next element of the stack (or queue)
-
  *
-
  * Description: doubly linked list node structure
-
  * for stack, queues, LIFO, FIFO Holberton project
-
  */
 
 typedef struct stack_s
@@ -47,19 +39,12 @@ typedef struct stack_s
 } stack_t;
 
 /**
-
  * struct bus_s - variables -args, file, line content
-
  * @arg: value
-
  * @file: pointer to monty file
-
  * @content: line content
-
  * @lifi: flag change stack <-> queue
-
  * Description: carries values through the program
-
  */
 
 typedef struct bus_s
@@ -79,19 +64,12 @@ typedef struct bus_s
 extern bus_t bus;
 
 /**
-
  * struct instruction_s - opcode and its function
-
  * @opcode: the opcode
-
  * @f: function to handle the opcode
-
  *
-
  * Description: opcode and its function
-
  * for stack, queues, LIFO, FIFO Holberton project
-
  */
 
 typedef struct instruction_s
